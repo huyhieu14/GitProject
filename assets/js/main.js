851c75847e183952ae3a03da13547e568d1d8af6
huyhieu14 <83973708+huyhieu14@users.noreply.github.com>
2021-12-27 21:07:53 +0700

    dom

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,19 @@ function convert() {
   let celsius = document.getElementById("celsius").value;
   let fahrenheit = celsius * 1.8 + 32;
   document.getElementById("result").value = fahrenheit;
-  console.log(celsius);
-  console.log(fahrenheit);
+}
+
+function sum() {
+    let a = parseInt(document.getElementById("numberA").value);
+    let b = parseInt(document.getElementById("numberB").value);
+    let sum = a + b;
+    document.getElementById("resultSum").value = sum;
+}
+
+
+function getInfor() {
+  let name = document.getElementById("name").value;
+  let add = document.getElementById("address").value;
+  let date = document.getElementById("date").value;
+  document.getElementById("resuilt").innerHTML = "Name: "+name + "- Address: " + add + "- Date: " +  date;
 }
